c0ea2e2b37ba45f9d93ef44be5ce3603cd5c55b1
Vaishnavi Chillara <vaishnavichillara018@gmail.com>
2026-05-21 14:49:04 +0530

    update profile

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
-
+import cookieParser from "cookie-parser";
 
 
 const app=express();
@@ -13,6 +13,8 @@ const __dirname=path.resolve();
 const PORT=ENV.PORT||3000;
 
 app.use(express.json()); //middleware to parse incoming JSON data   (req.body)
+app.use(cookieParser());
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 //make ready for deployment
