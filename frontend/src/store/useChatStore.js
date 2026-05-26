6d2eee34344d3ce55ab5e3c1bef8c6b74ce244b5
Vaishnavi Chillara <vaishnavichillara018@gmail.com>
2026-05-26 16:14:47 +0530

    chat container setup implemented

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -42,6 +42,17 @@ export const useChatStore=create((set,get) => ({
         }finally{
             set({isUserLoading:false})
         }
+    },
+    getMessagesByUserId:async(userId)=>{
+        set({isMessagesLoading:true});
+        try {
+            const res=await axiosInstance.get(`/messages/${userId}`);
+            set({messages:res.data});
+        } catch (error) {
+            toast.error(error.response?.data?.message || "Failed to fetch messages");
+        }finally{
+            set({isMessagesLoading:false})
+        }
     }
 
 }));
